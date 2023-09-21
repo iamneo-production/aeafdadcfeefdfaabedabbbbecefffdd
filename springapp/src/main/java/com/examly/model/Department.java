2a484fb1194c7a8bd9b8bcb9fa3efa2a21d357b7
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:27:03 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/model/Department.java
+++ springapp/src/main/java/com/examly/model/Department.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
+
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
